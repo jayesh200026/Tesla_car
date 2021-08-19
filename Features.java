bb82123d1086b0131e4a29738a2bf7290ae4de65
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-19 20:30:44 +0530

    Added the motion detetction feture of the car

--- Features.java
+++ Features.java
@@ -2,5 +2,9 @@ public class Features{
 public  static void main(String args[])
 {
 System.out.println("Tesla Model X");
+
+public void motion_detection(){
+System.out.println("Motion detetction feature of the car");
+}
 }
 } 
